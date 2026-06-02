08a20026bb7481193f0bb26b75c803c9d06905c5
moutai <moutai@moutaideMacBook-Pro.local>
2026-06-02 21:59:04 +0800

    Fix: build errors, responsive types, HashRouter

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hsall/',
+  base: './',
   plugins: [react()],
   server: {
     port: 5173,
