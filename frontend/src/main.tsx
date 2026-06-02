240ff9fda4a2adc8041350c31d90547c94a92895
moutai <moutai@moutaideMacBook-Pro.local>
2026-06-02 21:18:21 +0800

    Fix: Switch to HashRouter for GitHub Pages SPA

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import App from './App';
@@ -14,9 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         borderRadius: 6,
       },
     }}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </ConfigProvider>
   </React.StrictMode>
 );
